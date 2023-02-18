d72eccfc38231b22da38b47f59517300634df1c5
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-18 19:04:01 +0300

    Задача 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int des = 10; 
+if( num / 100 == 0)
+    Console.WriteLine("Третьей цифры нет");
+else{
+while(num / des != 0)
+{
+    des = des * 10;
+}
+int result = (num % (des / 100)) / (des / 1000);
+Console.WriteLine($"Третья цифра {result}");
+}
