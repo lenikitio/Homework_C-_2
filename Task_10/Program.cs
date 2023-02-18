46f402b472d1b3998dad23e1fba66c9dc60c968a
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-18 18:16:30 +0300

    Задача 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if( num < 100 || num > 999)
+{
+    Console.WriteLine("Это не трёхзначное число, попробуйте ещё раз");
+}
+else
+{
+    int secondValue = (num % 100) / 10;
+    Console.WriteLine($"Среднее число: {secondValue}");
+}
+
