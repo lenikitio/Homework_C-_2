d267ce82241ba970df5baed4266d7c845ed7fc4d
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-19 15:31:45 +0300

    Задача 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели цифрой ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 8 && num > 0)
+{
+    if(num > 0 && num < 6)
+    Console.WriteLine("Это будний день");
+    else
+    Console.WriteLine("Это выходной");
+}
+else
+{
+    Console.WriteLine("Перебор, нужно число от 1 до 7");
+}
